cff3fbd8f0975ed62f9e2c945fef2835d5e1371e
Lachlan <42135069+lachlanelijah@users.noreply.github.com>
2024-03-16 17:48:13 +1100

    Initial Commit

---
+++ DialogueTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DialogueTest
+//
+//  Created by Lachlan Atack on 16/3/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DialogueTest/DialogueTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DialogueTestApp.swift
+//  DialogueTest
+//
+//  Created by Lachlan Atack on 16/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct DialogueTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
